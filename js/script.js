ceaf483e686cbb937245c5e0159630ccfb172e5d
gjj52019 <garyjjenks@verizon.net>
2021-09-14 11:48:11 -0400

    added the paragraph elements and ids to html and cached them all in the script file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log($)
+//cached items 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const $state = $('#state');
+const $billTitle = $('#bill-title');
+const $billNumber = $('#bill-number');
+const $lastActionDate = $('#last-action-date');
+const $lastAction = $('#last-action');
+const $billText = $('bill-text');
+const $querySearched = $('query-searched')
+const $input = $('input[type="text"]');
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
