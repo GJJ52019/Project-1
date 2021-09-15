f1ad1ced0ba7705d11fde728566a723db82f2be0
gjj52019 <garyjjenks@verizon.net>
2021-09-15 15:17:27 -0400

    replaced building the main with paragraphs to building a table with search results, as a result removed a majority of cached items, added code to remove text from input area on search, now searches for up to 10 bills related to the query

--- js/script.js
+++ js/script.js
@@ -1,16 +1,11 @@
 //cached items 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-const $state = $('#state');
-const $billTitle = $('#bill-title');
-const $billNumber = $('#bill-number');
-const $lastActionDate = $('#last-action-date');
-const $lastAction = $('#last-action');
-const $billText = $('#bill-text');
 const $querySearched = $('#query-searched')
-const $input = $('input[type="text"]');
+let $input = $('input[type="text"]');
 let $stateID = "";
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+//handles the information in the selection box
 const select = document.getElementById('edit-state-id')
 select.addEventListener('change', function () {
     let index = select.options[select.selectedIndex].value;
@@ -21,18 +16,13 @@ function handleGetData(event) {
     // prevents the page from reloading on default
     event.preventDefault();
 
-
-
     //getting the user input
     userInput = $input.val();
-    $querySearched.text(userInput);
+    $querySearched.text("Recent Bills Regarding " + userInput);
 
-
-
+    //gets the ajax and if good criteria then pulls the data requested from the render function
     $.ajax({
         url: 'https://api.legiscan.com/?key=064d811d52b70be6cd88415100845f4f&op=search&state='+ $stateID +'&query=' + userInput
-        //'https://www.omdbapi.com/?apikey=1ab46e65&t=' + userInput
-
     }).then(
         (data) => {
             render(data);
@@ -44,15 +34,26 @@ function handleGetData(event) {
 
 }
 
+//grabs the information to display from cached items
 function render(data) {
+    $("#tbody").empty();
+    for(let i =0; i<10;i++){
+        const template =`
+        <tr>
+            <td><strong>State: </strong>${data.searchresult[i].state}</td>
+            <td><strong>Bill Title: </strong>${data.searchresult[i].title}</td>
+            <td><strong>Bill Number: </strong>${data.searchresult[i].bill_number}</td>
+            <td><strong>Date of Last Action: </strong>${data.searchresult[i].last_action_date}</td>
+            <td><strong>Last Action Taken: </strong>${data.searchresult[i].last_action}</td>
+            <td><strong>Bill Text URL: </strong><a href="${data.searchresult[i].text_url}" target="_blank">${data.searchresult[i].text_url}</a></td>
+        </tr>
+    `
+    //appened the info
+    $('tbody').append(template);
+    };
 
-    $state.text('State: ' + data.searchresult[0].state);
-    $billTitle.text('Bill Title: ' + data.searchresult[0].title);
-    $billNumber.text('Bill Number: ' + data.searchresult[0].bill_number);
-    $lastActionDate.text('Date of Last Action: ' + data.searchresult[0].last_action_date);
-    $lastAction.text('Last Action Taken: ' + data.searchresult[0].last_action);
-    $billText.html(`Bill Text URL: <a href="${data.searchresult[0].text_url}" target="_blank">${data.searchresult[0].text_url}</a>`);
-
+        //clears the input search area
+   $('#input-area').val('')
 }
 
 //targets form in html and on submit runs handleGetData
